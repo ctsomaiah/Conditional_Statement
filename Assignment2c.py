c18ffa4bdf011c7d2d6c67b94f506da972c38f14
ctsomaiah <ctsharath14@gmail.com>
2023-01-21 00:06:17 +0530

    Added: Program to find if a number is odd or even

---
+++ Assignment2c.py
@@ -0,0 +1,10 @@
+# Assignment - Conditional statements
+
+# 3. Write a Python program to find whether a given number (accepted by the user) is even or odd
+
+print("Enter any number")
+x = int(input())
+if x%2==0:
+    print("It is a even number")
+else:
+    print("It is a odd number")
