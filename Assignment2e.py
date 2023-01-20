32efb74b435a01f4d21ca34f14068bdf18272d89
ctsomaiah <ctsharath14@gmail.com>
2023-01-21 00:10:11 +0530

    Added: Program that returns the greatest of 3 numbers

---
+++ Assignment2e.py
@@ -0,0 +1,19 @@
+# Assignment - Conditional statements
+
+# 5. Write a python program that returns the greatest of 3 numbers
+#     Inputs:
+#         a = 200
+#         b = 768
+#         c = 555
+#     Expected Output:
+#         B is greater
+
+a = 200
+b = 768
+c = 555
+if a>b and a>c:
+    print("A is greater")
+elif b>a and b>c:
+    print("B is Greater")
+else:
+    print("C is greater")
