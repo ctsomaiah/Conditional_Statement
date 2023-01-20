bd9924a51e261e5e6cd9ed360ebae3ed6a9d9ed3
ctsomaiah <ctsharath14@gmail.com>
2023-01-21 00:11:28 +0530

    Added: Program to check the data type of the variable

---
+++ Assignment2f.py
@@ -0,0 +1,13 @@
+# Assignment - Conditional statements
+
+# 6. a = 'string', check the data type of the variable 'a', if it is string print 'string' else print 'No'
+
+a = 'string'
+if type(a)==str:
+    print("String")
+else:
+    print("No")
+
+
+
+
